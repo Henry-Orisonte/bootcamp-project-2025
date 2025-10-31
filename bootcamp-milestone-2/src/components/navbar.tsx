9b2a2ee38b5c6e057d27120b0ed0a359626e4a7d
Henry Orisonte <henryorisonte@MacBook-Pro-59.local>
2025-10-30 21:11:33 -0700

    feat: added navbar code from milestone 1

---
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+// @ts-ignore
+import style from "./navbar.module.css";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    // replace everything in between the <header> & <header /> tags
+    // with your navbar code from your earlier milestones
+    // NOTE: we use "class" in HTML but React is quirky so we have to
+    // change all the "class" to "className"
+    <header className={style.navbar}>
+      <h1>Henry's Personal Website</h1>
+      <nav>
+        <ul className={style.navList}>
+          <li>
+            <Link className={style.navLink} href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className={style.navLink} href="/blogs">
+              Blogs
+            </Link>
+          </li>
+          <li>
+            <Link className={style.navLink} href="/resume">
+              Resume
+            </Link>
+          </li>
+          <li>
+            <Link className={style.navLink} href="/about">
+              Contact Me
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}

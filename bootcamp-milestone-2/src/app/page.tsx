dc7a370aa908e55d6f757d8ffa3ba565f2e23c1c
Henry Orisonte <henryorisonte@MacBook-Pro-59.local>
2025-10-30 21:23:27 -0700

    feat: Port over html for home page

--- bootcamp-milestone-2/src/app/page.tsx
+++ bootcamp-milestone-2/src/app/page.tsx
@@ -1,7 +1,42 @@
+import React from "react";
+import Navbar from "../components/navbar";
+import style from "./page.module.css";
+
 
 export default function Home() {
   return (
-    <div>
-    </div>
+    <>
+      <Navbar />
+      <main className={style.main}>
+        <h1 className={style.pageTitle}>About Me</h1>
+        <div className={style.about}>
+          <div className={style.aboutImage}>
+            <img
+              src="/polyimg.jpg.avif"
+              alt="Image of Cal Poly SLO campus"
+              width={500}
+              height={400}
+            />
+          </div>
+          <div className={style.aboutText}>
+            <p>
+              Hello, my name is <strong>Henry Orisonte</strong> and I am a third
+              year transfer at <strong>Cal Poly</strong>.
+            </p>
+            <p>
+              A few things that you should know about me are that I enjoy watching
+              movies, shows, and basketball. I recently finished rewatching
+              Regular Show, one of my favorite shows, along with Avatar: The Last
+              Airbender. I also enjoy baking, especially cinnamon rolls and
+              banana bread.
+            </p>
+          </div>
+        </div>
+      </main>
+      <footer className={style.footer}>
+        © 2023 Henry's Personal Website | All Rights Reserved
+      </footer>
+    </>
+
   );
 }

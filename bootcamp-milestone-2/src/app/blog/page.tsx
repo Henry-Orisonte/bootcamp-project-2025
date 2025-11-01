48499e6f9714e4a889471de3ed2006afb31afbbc
Henry Orisonte <henryorisonte@MacBook-Pro-59.local>
2025-10-31 20:40:19 -0700

    added navbar to blog page

--- bootcamp-milestone-2/src/app/blog/page.tsx
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import Navbar from "../../components/navbar";
 import blogs from "../blogData";
-import BlogPreview from '../../components/blogPreview';
+import Link from "next/link";
+import BlogPreview from "../../components/blogPreview";
 // @ts-ignore
-import styles from "./page.module.css"; 
+import style from "./page.module.css";
+
 export default function BlogPage() {
   return (
-    <main>
-      <h1>Blog</h1>
-      {blogs.map((blog) => (
-        <BlogPreview {...blog} /> 
-      ))}
-    </main>
+    <>
+      <Navbar />
+      <main className={style.main}>
+        <h1 className={style.pageTitle}>Blog</h1>
+        {blogs.map((blog) => (
+          <BlogPreview key={blog.slug} {...blog} /> 
+        ))}
+      </main>
+      </>
   );
 }
